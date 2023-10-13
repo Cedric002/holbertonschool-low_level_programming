002480abdb3c42c8e4a24e51281e4c00e16b8c74
Cedric002 <cedgnleblanc@gmail.com>
2023-10-13 06:20:23 -0700

    printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
