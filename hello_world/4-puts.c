6573cdb10ec8b525d4ed3a9bf2140a6834d4f6e0
Cedric002 <cedgnleblanc@gmail.com>
2023-10-14 08:16:03 -0700

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-#!/bin/bash
-# Simply a wrapper script to keep you from having to use betty-style
-# and betty-doc separately on every item.
-# Originally by Tim Britton (@wintermanc3r), multiargument added by
-# Larry Madeo (@hillmonkey)
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
