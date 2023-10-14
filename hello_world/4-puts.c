26f5f839ae2baa2e1d109542e19da2a661a69b2d
Cedric002 <cedgnleblanc@gmail.com>
2023-10-14 08:12:39 -0700

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: Always 0.
- */
+#!/bin/bash
+# Simply a wrapper script to keep you from having to use betty-style
+# and betty-doc separately on every item.
+# Originally by Tim Britton (@wintermanc3r), multiargument added by
+# Larry Madeo (@hillmonkey)
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
