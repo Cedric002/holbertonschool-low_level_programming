49690f7c7d559a88ea0a155cf50b6f39e50cb344
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 10:20:49 -0700

    Dernier chiffre et comparaison

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Print the last digit of a randomly generated number
+ * 	and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
 */
