1214a74a35cbf61b93fa31dc2dd5a83ad8e2904c
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 06:58:36 -0700

    dernier chiffre et comparaison

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,14 +23,13 @@ int main(void)
                 printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
         }
-        else if ((n % 10) = 0)
+        else if ((n % 10) < 6 && !=0)
         {
-                printf("Last digit of %d is %d  and is 0\n", n, n % 10);
+                printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, n % 10);
         }
-        else ((n % 10) < 6)
+        else
         {
-                printf("Last digit of %d is %d and il less than 6 and not
-			0\n", n, n % 10);
+                printf("Last digit of %d is %d and is 0\n", n, n % 10);
         }
 
 	return (0);
