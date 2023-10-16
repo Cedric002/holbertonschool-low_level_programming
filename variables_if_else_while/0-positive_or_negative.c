4b0fb654c7acb6001fc0138a6bb1ebdb1e0871d4
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 04:15:44 -0700

    Nombre aleatoire poistif ou negatif

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -22,15 +22,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf(" is positive \n");
+		printf("%d" "is positive \n");
 	}
 	else if (n < 0)
 	{
-		printf(" is negative \n");
+		printf("%d" "is negative \n");
 	}
 	else
 	{
-		printf(" is zero \n");
+		printf("%d" "is zero \n");
 	}
 
 	return (0);
