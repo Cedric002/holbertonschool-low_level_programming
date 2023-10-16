49fdb5f84bdde7b52c6d2894bbee0b66d512785c
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 07:14:41 -0700

    dernier chiffre et comparaison

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
 	if ((n % 10) > 5)
         {
                 printf("Last digit of %d is %d and is greater than 5\n",
