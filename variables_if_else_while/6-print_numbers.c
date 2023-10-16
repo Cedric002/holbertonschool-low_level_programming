9a35699e474b7a0ac0d650b0db5447f745f270c4
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 13:42:53 -0700

    Afficher chiffres en base 10 en partant de 0

---
+++ variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
