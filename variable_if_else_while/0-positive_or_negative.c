b30111e39c203b01bcba98f91226e302c4de3bdd
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 02:22:56 -0700

    Nombre aleatoire positif ou negatif

--- variable_if_else_while/0-positive_or_negative.c
+++ variable_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -11,8 +10,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("%d \n", n);
-	
 	if (n > 0)
 	{
 		printf("C'est positif.");
