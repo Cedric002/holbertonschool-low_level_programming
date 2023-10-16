65242724bbb3358c4b1d7a4d82f35ae01561afff
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 07:13:06 -0700

    dernier chiffre et comparaison

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
