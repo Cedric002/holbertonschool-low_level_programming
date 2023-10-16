2e85932ec26f924c9d44b72d05841e2b0a3b5cc6
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 05:48:41 -0700

    Nombre aléatoire positif ou negatif

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,19 +18,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	printf("%d", n);
-
 	if (n > 0)
 	{
-		printf("n is positive\n");
+		printf("%d is positive\n");
 	}
 	else if (n < 0)
 	{
-		printf("n is negative\n");
+		printf("%d is negative\n");
 	}
 	else
 	{
-		printf("n is zero\n");
+		printf("%d is zero\n");
 	}
 
 	return (0);
