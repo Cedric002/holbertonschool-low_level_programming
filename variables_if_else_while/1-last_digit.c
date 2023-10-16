80c91cb86754b5ce52f98b9bfc87efde69b6247b
Cedric002 <cedgnleblanc@gmail.com>
2023-10-16 07:19:17 -0700

    dernier chiffre et comparaison

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
@@ -16,8 +17,7 @@ int main(void)
 	/* your code goes there */
 	if ((n % 10) > 5)
         {
-                printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
+                printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
         }
         else if ((n % 10) < 6 && (n % 10) != 0)
         {
