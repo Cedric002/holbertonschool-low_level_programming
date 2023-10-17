bf9e7cbba1764e61c5ce7ec18e8e48e8b101f8c0
Cedric002 <cedgnleblanc@gmail.com>
2023-10-17 05:35:54 -0700

    afficher _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+        return (0);
+}
