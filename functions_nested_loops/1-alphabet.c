137de52d247d152e68be7fe721b8f0022b519267
Cedric002 <cedgnleblanc@gmail.com>
2023-10-17 06:13:53 -0700

    afficher alphabet avec _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char lettre;
+
+        for (lettre = 'a'; lettre <= 'z'; lettre++)
+                putchar(letter);
+
+        putchar('\n');
+
+        return (0);
+}
--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -7,5 +7,6 @@
  */
 int main(void)
 {
+    print_alphabet();
     return (0);
 }
