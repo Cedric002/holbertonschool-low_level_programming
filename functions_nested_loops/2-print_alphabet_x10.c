5e4f414204762c91ad41928d0850a27eb4e6ddef
Cedric002 <cedgnleblanc@gmail.com>
2023-10-18 06:35:02 -0700

    Afficher alphabet minuscule 10 fois

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	char lettre;
+
+	for (lettre = 0; lettre < 10; lettre) {
+        	for (lettre = 'a'; lettre <= 'z'; lettre++) {
+                	putchar(letter);
+		}
+
+        	putchar('\n');
+	}
+
+	return (0);
+}
