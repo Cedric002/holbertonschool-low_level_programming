c188c0f24ab674fa622e5ba08a69683aa04e24e8
Cedric002 <cedgnleblanc@gmail.com>
2023-10-19 13:58:28 -0700

    Afficher le signe d'un nombre

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "main.h"
 
 /**
  * print_sign - Entry point
- * @c: entry point
- * Return: si characteres en minuscule afficher 1 sinon 0
+ * @n: entry point
+ * Return: afficher signe des nombre
  */
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	return (0);
