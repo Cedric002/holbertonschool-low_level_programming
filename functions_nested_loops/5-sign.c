a9f14ab71976feee0544671664f7a16cdca23b33
Cedric002 <cedgnleblanc@gmail.com>
2023-10-19 13:46:42 -0700

    Afficher le signe d'un nombre

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
 	}
 	else if (c == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return (0);
 	}
 	else if (c < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		return (-1);
 	}
 	return (0);
