cdab3cbef96f88ca0ee62c872b59898253b9eeb2
Cedric002 <cedgnleblanc@gmail.com>
2023-10-19 06:43:06 -0700

    Verifier le caratere alphabetique

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+/**
+ * _isalpha - Entry point
+ * @c: entry point
+ * Return: si characteres en minuscule afficher 1 sinon 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
