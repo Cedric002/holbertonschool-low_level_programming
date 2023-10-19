b831ae1c9d494120031ea3532f4b26c5e4585750
Cedric002 <cedgnleblanc@gmail.com>
2023-10-19 05:56:03 -0700

    Si minuscule return 1 sinon return 0

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * int _islower(int c) - Entry point
+ * @int c
+ * Return: si characteres en minuscule afficher 1 sinon 0
+ */
+int _islower(int c);
+{
+	scanf("%c", &c);
+	
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
