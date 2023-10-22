17091024a6099c34c83c65e9d11e5fa80a8b4cab
Cedric002 <cedgnleblanc@gmail.com>
2023-10-22 09:00:50 -0700

    verfier un chiffre de 0 a 9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
 
 /**
-* _isdigit - entry
-* @c:
-* Return:  0 or 1
-**/
+ * _isdigit - entry
+ * @c: prototype
+ * Return:  0 or 1
+ **/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
