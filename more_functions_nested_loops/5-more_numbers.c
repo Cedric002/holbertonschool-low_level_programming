7b3686899815d1393b887153ede24ae56f732594
Cedric002 <cedgnleblanc@gmail.com>
2023-10-22 09:52:29 -0700

    afficher 0 a 14, 10 fois

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * more_numbers - entry
+ *
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				putchar('1');
+			putchar(j % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
