2062619f9feb3d552d40f294a5887436cb6f0e2f
Cedric002 <cedgnleblanc@gmail.com>
2023-10-24 01:57:55 -0700

    Afficher string + new ligne

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -11,6 +11,6 @@
  */
 void _puts(char *str)
 {
-	str = "I do not fear computers I fear the lack of them - Isaac Asimov\n";
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov\n";
 	puts(str);
 }
