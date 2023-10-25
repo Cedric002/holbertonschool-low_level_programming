f5f2bb38e5238522cca3d8d2fa82e06f0cd77870
Cedric002 <cedgnleblanc@gmail.com>
2023-10-25 07:00:31 -0700

    Afficher un caractere 1/2 sur une chaine

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * puts2 - entry
+ * @str: string
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+       
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,3 +9,5 @@ void _puts(char *str);
 void print_rev(char *s);
 
 void rev_string(char *s);
+
+void puts2(char *str);
