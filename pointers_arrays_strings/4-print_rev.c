a54c1bb95e94b79ceafad3b615c1a562493a1d70
Cedric002 <cedgnleblanc@gmail.com>
2023-10-25 06:27:47 -0700

    Afficher string inversé + new ligne

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,14 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
+	int index;
 
-	while (s[index++])
+        while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
-	putchar(s[index]);
+		for (index = len - 1; index >= 0; index--)
+		putchar(s[index]);
 
-	putchar('\n');
+		putchar('\n');
 }
