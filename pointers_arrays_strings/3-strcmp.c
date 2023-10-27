0bd64bef8ca0850a5e31c2b51728a322a5309b5d
Cedric002 <cedgnleblanc@gmail.com>
2023-10-27 05:23:05 -0700

    Comparer 2 strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,26 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	int flag = 0;
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 == '\0' || *s2 == '\0')
+		if (s1[i] != s2[i])
+		{
+			flag = 1;
+		}
 
-			s1++;
-		s2++;
+		i++;
+
 	}
+	if (s1[i] != '\0' || s2[i] != '\0')
+		return (1);
 
-	if (*s1 == '\0' && *s2 == '\0')
+	if (flag == 0)
 		return (0);
+
 	else
-		return (-1);
+
+		return (1);
 }
