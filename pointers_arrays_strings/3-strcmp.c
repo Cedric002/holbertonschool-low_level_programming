734a20edfde747ca5cdc41416c5fe7c503ddd992
Cedric002 <cedgnleblanc@gmail.com>
2023-10-27 02:23:33 -0700

    Comparer 2 string

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _strcmp - entry
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: copy
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0' || *s2 == '\0')
+
+			s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	else
+		return (-1);
+}
