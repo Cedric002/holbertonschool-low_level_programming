4e98c52258d87d832e398bd58c031859c23b8e50
Cedric002 <cedgnleblanc@gmail.com>
2023-10-27 00:58:39 -0700

    Copier un string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _strncpy - entry
+ * @dest: string 1
+ * @src: string 2
+ * @n: value
+ *
+ * Return: copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	for (i = 0 ; i < n ; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,3 +17,5 @@ void puts_half(char *str);
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
