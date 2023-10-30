590b5e0a0cc9719100c03d5ca3ce7f349c4773b4
Cedric002 <cedgnleblanc@gmail.com>
2023-10-30 02:34:46 -0700

    Afficher string en Recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - affiche un string (Recursion)
+ * @s: string
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	printf("%c", *s);
+}
