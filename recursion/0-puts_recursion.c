61a4a87597fb8af131f65120020af5538a31a106
Cedric002 <cedgnleblanc@gmail.com>
2023-10-30 07:09:35 -0700

    Afficher string a l'envers (Recursion)

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -13,10 +13,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
 		return;
-		_putchar('\n');
 	}
+	_putchar(*s);
+	s++;
+	_putchar('\n');
+
 	_puts_recursion(s);
 }
