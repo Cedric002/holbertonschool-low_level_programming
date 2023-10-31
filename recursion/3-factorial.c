95e49601b30849fb24ddb7de6f7335a681aa13a5
Cedric002 <cedgnleblanc@gmail.com>
2023-10-31 02:58:31 -0700

    Factorielle

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -12,11 +12,18 @@
 int factorial(int n)
 {
 	if (n > 0)
+	{
 		return (n * factorial(n - 1));
+	}
 
 	else if (n == 0)
+	{
 		return (1);
+	}
 
 	else if (n < 0)
+	{
 		return (-1);
+	}
+	return (n);
 }
