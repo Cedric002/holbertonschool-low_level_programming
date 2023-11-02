d328d4e4adcb140d59e6ed9409e7a89fe3dfbcfa
Cedric002 <cedgnleblanc@gmail.com>
2023-11-02 02:48:00 -0700

    Afficher nom programme

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * @argc: argument
+ * @argv: tableau
+ *
+ * Return: mynameis
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
