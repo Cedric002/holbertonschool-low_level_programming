cf3f8d19da9c702e3463cac9d4ace39bccb1a767
Cedric002 <cedgnleblanc@gmail.com>
2023-11-02 08:16:30 -0700

    envoyer des arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * @argc: argument
+ * @argv: tableau
+ *
+ * Return: nb argument
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	for (int i = 1; i < argc; i++)
+	{
+		printf("%d: [%s]\n", i, argv[i]);
+	}
+
+	return (0);
+}
