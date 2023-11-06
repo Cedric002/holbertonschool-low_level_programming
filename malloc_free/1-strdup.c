9c022bbf52a71d7f9ac27b9b911aedfbf1da15c1
Cedric002 <cedgnleblanc@gmail.com>
2023-11-06 07:27:34 -0800

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _strdup - entry
+ * @str: string
+ *
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int length = 0;
+	char *copy = malloc((length + 1) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int i = 0; i <= length; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	return (copy);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,1 +1,3 @@
 char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
