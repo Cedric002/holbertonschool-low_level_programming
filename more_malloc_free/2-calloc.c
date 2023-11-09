e7c9dbd7788c40a0924c97cca8cb6b5b017a23c0
Cedric002 <cedgnleblanc@gmail.com>
2023-11-09 06:40:58 -0800

    Calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -19,18 +19,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	void *ptr = malloc(nmemb * size);
+	char *ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	char *byte = (char *)ptr;
-
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < nmemb; i++)
 	{
-		byte[i] = 0;
+		*(ptr + (i * size)) = 0;
 	}
 
 	return (ptr);
