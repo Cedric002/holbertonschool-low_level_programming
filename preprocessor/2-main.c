bee91ce16ef85539312d5c7668a0f2932170dc64
Cedric002 <cedgnleblanc@gmail.com>
2023-11-10 00:40:04 -0800

    nom fichier

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
+}
