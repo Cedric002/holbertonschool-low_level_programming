57b8f932af1ab157ca43408bc9a29427edb9f766
Cedric002 <cedgnleblanc@gmail.com>
2023-11-14 02:32:56 -0800

    create new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "dog.h"
+/**
+ * new_dog - entry
+ * @name: nom du nouveau chien
+ * @age: age du nouveau chien
+ * @owner: proprietaire
+ *
+ * Return: 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t *d;
+
+	d = malloc(sizeof(struct dog));
+
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
+	{
+		return (NULL);
+	}
+}
