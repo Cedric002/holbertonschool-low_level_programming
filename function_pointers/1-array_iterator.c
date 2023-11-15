bdb7fa80a14d0a7fd84764fd94aca4f165466db4
Cedric002 <cedgnleblanc@gmail.com>
2023-11-15 02:32:38 -0800

    array iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - fonction en paametre sur chaque elements tableau
+ * @array: tableau
+ * @size: taille
+ * @action: pointeur de fonction
+ *
+ * Return: name
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
