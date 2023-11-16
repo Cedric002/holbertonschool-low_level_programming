4fa9e4d315717c8e890a2e5a4d7fdc35a7c6b98d
Cedric002 <cedgnleblanc@gmail.com>
2023-11-16 00:28:49 -0800

    cherche un entier

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,18 +13,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (cmp != 0)
-	{
-		cmp -> element
-	}
-
-	if (cmp != element)
-	{
-		return (-1);
-	}
+	int i;
 
 	if (size <= 0)
 	{
 		return (-1);
 	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
 }
