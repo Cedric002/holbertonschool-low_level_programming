dc360eb3f096e79347d58339c700c15fcaa01c48
Cedric002 <cedgnleblanc@gmail.com>
2023-11-17 02:16:55 -0800

    somme variadic function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,19 +12,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum = 0;
+
 	va_list args;
 
 	va_start(args, n);
 
-	unsigned int i;
-	int sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-		{
-			return (0);
-		}
 		sum += va_arg(args, int);
 	}
 
