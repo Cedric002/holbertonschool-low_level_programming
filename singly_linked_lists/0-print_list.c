27a63bf8852446930a39cadaea9e7032697ccfd9
Cedric002 <cedgnleblanc@gmail.com>
2023-11-27 06:34:51 -0800

    afficher tous les elements d'une liste

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,24 +12,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
-	while (h)
+	if (h == NULL)
 	{
-
-		if (h == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-
-		h = h->next;
-		count++;
+		printf("[0] (nil)\n");
+		return (0);
 	}
 
-	return (count);
+	while (h != NULL)
+	{
+		printf("%s\n", h->str);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
 }
