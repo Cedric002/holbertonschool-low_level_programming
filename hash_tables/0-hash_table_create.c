9ddc87b84c5725a38fc4a8cf178f700ca0dbfb26
Cedric002 <cedgnleblanc@gmail.com>
2023-12-06 02:32:21 -0800

    Creer un hash Table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,26 +10,23 @@
 
 hash_table_t *hash_table_create(unsigned long size)
 {
-	if (size < 0)
-		return (NULL);
+	hash_table_t *ht;
 
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
+	{
 		return (NULL);
+	}
 
-	ht->size = size;
-	ht->array = malloc(sizeof(void *) * size);
+	ht->array = malloc(size * sizeof(hash_node_t *));
 
 	if (ht->array == NULL)
 	{
-		free(ht);
 		return (NULL);
 	}
 
-	for (size_t i = 0; i < ht->size; i++)
-	{
-		ht->array[i] = NULL;
-	}
+	ht->size = size;
+
 	return (ht);
 }
