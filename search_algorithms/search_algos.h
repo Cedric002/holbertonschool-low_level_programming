2bafdfa2de747ab527d3075a0efd65ff443b0627
Cedric002 <cedgnleblanc@gmail.com>
2024-01-17 01:48:09 -0800

    aout du fichier search algo.h

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef ALGO
+#define ALGO
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
